2c4eb393588526f052edb392f7d44850a651ebaf
Isackson99 <is.serrano@duocuc.cl>
2023-05-24 02:40:32 -0400

    Logré que funcionara, no se como XD

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,14 +4,14 @@ from django.db.models.signals import post_save
 from .models import Profile
 
 @receiver(post_save, sender=Profile)
-def add_user_to_usuario_group(sender, instance, created, **kwargs):
+def add_user_to_students_group(sender, instance, created, **kwargs):
     if created:
         try:
-            usuario = Group.objects.get(name='cliente')
+            students = Group.objects.get(name='cliente')
         except Group.DoesNotExist:
-            usuario = Group.objects.create(name='estudiante')
-            usuario = Group.objects.create(name='vendedor')
-            usuario = Group.objects.create(name='bodeguero')
-            usuario = Group.objects.create(name='tesorero')
-            usuario = Group.objects.create(name='administrador')
-        instance.user.groups.add(usuario)
+            students = Group.objects.create(name='cliente')
+            students = Group.objects.create(name='bodeguero')
+            students = Group.objects.create(name='vendedor')
+            students = Group.objects.create(name='contador')
+            students = Group.objects.create(name='administrativo')
+        instance.user.groups.add(students)
--- django_MusicPro/settings.py
+++ django_MusicPro/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
     'core.apps.CoreConfig',
     'django.contrib.humanize',
     'rest_framework',
-    'accounts.apps.AccountsConfig',
     
 ]
 
